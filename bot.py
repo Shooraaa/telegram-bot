d65e7a6304dad929fd7caad566fa32bb77607e55
Shooraaa <designingbyshoora@gmail.com>
2026-04-20 00:01:08 +0530

    Change TOKEN to hardcoded value and update log messages
    Updated TOKEN assignment to a hardcoded value and modified logging messages.

--- bot.py
+++ bot.py
@@ -1,24 +1,19 @@
-import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.getenv(8670521053:AAG9hqMCoLGQza9P5OJ8MLqgmcOIYlKULYo)
+TOKEN = "8670521053:AAG9hqMCoLGQza9P5OJ8MLqgmcOIYlKULYo"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is working 🚀")
 
 def main():
-    if not TOKEN:
-        print("❌ BOT_TOKEN missing")
-        exit()
-
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    print("✅ Bot started")
+    print("Bot started...")
     app.run_polling()
 
 if __name__ == "__main__":
